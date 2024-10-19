2a66d0ac6508e6598503b6daff915d1acdcd3de3
Tom Beihofer <tom@sirius23.de>
2024-10-19 16:19:43 +0200

    daily prediction test with GitHub Actions

--- src/dopro2_HEFTcom_challenge/main.py
+++ src/dopro2_HEFTcom_challenge/main.py
@@ -6,59 +6,60 @@ from loguru import logger
 
 from dopro2_HEFTcom_challenge.config import config_logger
 from dopro2_HEFTcom_challenge.pipeline import (
-    DataIngestionTrainingPipeline,
-    DataPreparationTrainingPipeline,
-    ModelEvaluationPipeline,
-    ModelTrainingPipeline
+    # DataIngestionTrainingPipeline,
+    # DataPreparationTrainingPipeline,
+    # ModelEvaluationPipeline,
+    # ModelTrainingPipeline
+    PredictionPipeline
 )
 
 
-STAGE_NAME_01: Final = "Data Ingestion stage"
+STAGE_NAME: Final = "Prediction"
 
 # set up logging
 config_logger()
 
 try:
-    logger.info(">>> stage {} started <<<", STAGE_NAME_01)
-    obj = DataIngestionTrainingPipeline()
-    obj.main()
-    logger.info(">>> stage {} completed <<<", STAGE_NAME_01)
+    logger.info(">>> stage {} started <<<", STAGE_NAME)
+    obj = PredictionPipeline()
+    obj.predict()
+    logger.info(">>> stage {} completed <<<", STAGE_NAME)
 except Exception as e:
     logger.exception(e)
     raise e
 
 
-STAGE_NAME_02: Final = "Data Preparation stage"
+# STAGE_NAME_02: Final = "Data Preparation stage"
 
-try:
-    logger.info(">>> stage {} started <<<", STAGE_NAME_02)
-    obj = DataPreparationTrainingPipeline()
-    obj.main()
-    logger.info(">>> stage {} completed <<<", STAGE_NAME_02)
-except Exception as e:
-    logger.exception(e)
-    raise e
-
-
-STAGE_NAME_03: Final = "Model training stage"
-
-try:
-    logger.info(">>> stage {} started <<<", STAGE_NAME_03)
-    obj = ModelTrainingPipeline()
-    obj.main()
-    logger.info(">>> stage {} completed <<<", STAGE_NAME_03)
-except Exception as e:
-    logger.exception(e)
-    raise e
+# try:
+#     logger.info(">>> stage {} started <<<", STAGE_NAME_02)
+#     obj = DataPreparationTrainingPipeline()
+#     obj.main()
+#     logger.info(">>> stage {} completed <<<", STAGE_NAME_02)
+# except Exception as e:
+#     logger.exception(e)
+#     raise e
 
 
-STAGE_NAME_04: Final = "Model evaluation stage"
+# STAGE_NAME_03: Final = "Model training stage"
 
-try:
-    logger.info(">>> stage {} started <<<", STAGE_NAME_04)
-    obj = ModelEvaluationPipeline()
-    obj.main()
-    logger.info(">>> stage {} completed <<<", STAGE_NAME_04)
-except Exception as e:
-    logger.exception(e)
-    raise e
+# try:
+#     logger.info(">>> stage {} started <<<", STAGE_NAME_03)
+#     obj = ModelTrainingPipeline()
+#     obj.main()
+#     logger.info(">>> stage {} completed <<<", STAGE_NAME_03)
+# except Exception as e:
+#     logger.exception(e)
+#     raise e
+
+
+# STAGE_NAME_04: Final = "Model evaluation stage"
+
+# try:
+#     logger.info(">>> stage {} started <<<", STAGE_NAME_04)
+#     obj = ModelEvaluationPipeline()
+#     obj.main()
+#     logger.info(">>> stage {} completed <<<", STAGE_NAME_04)
+# except Exception as e:
+#     logger.exception(e)
+#     raise e
